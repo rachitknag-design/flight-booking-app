1a07167b65ee2b68bfbd2b5f73f98daec6749edc
Rachit Nag <rachitknag@gmail.com>
2026-08-08 17:15:12 +0530

    Initial commit - Flight Booking App

---
+++ src/test/java/com/flightbookingapp/FlightBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
